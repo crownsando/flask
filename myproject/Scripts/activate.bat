8895aed12cb56b821cc48b9b70ebb1d19708f38a
CYMin <90306913+crownsando@users.noreply.github.com>
2023-07-10 17:37:30 +0900

    2023-07-10 last commit

--- myproject/Scripts/activate.bat
+++ myproject/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\ITStudy\myproject\myproject
+set VIRTUAL_ENV=C:\ITStudy\06_flask\myproject\myproject
 
 if not defined PROMPT set PROMPT=$P$G
 
